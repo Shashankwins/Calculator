23a4232166cfe0017c9219ae1fd644c6cabd72a4
Shashankwins <shashankbhardwajwins@gmail.com>
2023-10-31 22:55:15 +0530

    Added my approch to replace eval method

---
+++ myFunction.js
@@ -0,0 +1,58 @@
+let str="";
+str= prompt("Enter your Expression");
+let a="";
+let b="";
+let op="";
+let pos;
+let result=0;
+let num1;
+let num2;
+
+for(let i=0;i<str.length;i++)
+{
+    if(str[i]=='+' || str[i]=='-' || str[i]=='*' || str[i]=='/')
+    {
+        op=str[i];
+    }
+    else
+    {
+        continue;
+    }
+}
+
+pos=str.indexOf(op);
+
+for(let i=0;i<str.length;i++)
+{
+    if(i<pos)
+    {
+        a=a+str[i];
+    }
+    else if(i>pos)
+    {
+        b=b+str[i];
+    }
+}
+
+console.log(op);
+console.log(pos);
+console.log(a);
+console.log(b);
+num1= Number(a);
+num2= Number(b);
+
+switch(op){
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+        result = num1 / num2;
+        break;
+}
+console.log(result);
