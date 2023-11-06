d09b1b0f13db306d63c3507c15e4afff12a08fd6
Shashankwins <shashankbhardwajwins@gmail.com>
2023-11-06 15:02:40 +0530

    removerd eval and added my own function

--- Calculator/assets/js/calc.js
+++ Calculator/assets/js/calc.js
@@ -1,19 +1,63 @@
-let exp="";
-function show(val){
-    if(val!=='=')
-    {
-        exp= exp+val;
-        document.getElementById("screen").value=exp;
+let exp = "";
+function show(val) {
+    if (val !== '=') {
+        exp = exp + val;
+        document.getElementById("screen").value = exp;
     }
-    else if(val=='=')
-    {   
-        let result=eval(exp);
-        document.getElementById("screen").value="";
-        document.getElementById("screen").value=result;
+    else if (val == '=') {
+        exp = result(exp);
+        document.getElementById("screen").value = exp;
     }
 }
 
-function clr(){
-document.getElementById("screen").value="";
-exp="";
+function clr() {
+    document.getElementById("screen").value = "";
+    exp = "";
+
+}
+
+
+function result(str) {
+
+    let a = "";
+    let b = "";
+    let op = "";
+    let pos;
+    let ans = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/') {
+            op = str[i];
+            break;
+        }
+        else {
+            continue;
+        }
+    }
+
+    pos = str.indexOf(op);
+
+    for (let i = 0; i < str.length; i++) {
+        if (i < pos) {
+            a = a + str[i];
+        }
+        else if (i > pos && str[i] >= 0 && str[i] <= 9) {
+            b = b + str[i];
+        }
+    }
+
+    switch (op) {
+        case '+':
+            ans = Number(a) + Number(b);
+            break;
+        case '-':
+            ans = Number(a) - Number(b);
+            break;
+        case '*':
+            ans = Number(a) * Number(b);
+            break;
+        case '/':
+            ans = Number(a) / Number(b);
+            break;
+    }
+    return ans;
 }
