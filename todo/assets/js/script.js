58c6739f128771681e07d275da142bbe999cf474
Shashankwins <shashankbhardwajwins@gmail.com>
2023-11-15 19:05:11 +0530

    updated todo

--- todo/assets/js/script.js
+++ todo/assets/js/script.js
@@ -1,5 +1,6 @@
 let arr=[];
 let ul= document.querySelector('ul');
+let r=document.getElementsByClassName('remove');
 
 function save() {
     let str=document.getElementById('insert').value;
@@ -8,37 +9,34 @@ function save() {
     }
     else if(str!==""){
         let li= document.createElement('li');
-    ul.append(li);
-    ul.insertBefore(li, ul.childNodes[0])
-    let parentBox= document.createElement('div');
-    li.append(parentBox);
-    parentBox.className="parentBox";
-    let content= document.createElement('div');
-    parentBox.append(content);
-    content.className="content";
-    let listBtn= document.createElement('div');
-    parentBox.append(listBtn);
-    listBtn.className="listBtn";
-    let task= document.createElement('p');
-    content.append(task);
-    task.innerText=str;
-    let icon1= document.createElement('span');
-    listBtn.append(icon1);
-    icon1.className="icon1";
-    let tick= document.createElement('i');
-    icon1.append(tick);
-    tick.className="fa-solid fa-check fa-lg";
-    let icon2=document.createElement('span');
-    listBtn.append(icon2);
-    icon2.className="icon2";
-    let a=document.querySelector('span');
-    let remove= document.createElement('i');
-    icon2.append(remove);
-    remove.className="fa-solid fa-xmark fa-lg";
-    document.getElementById('insert').value="";
+        ul.insertBefore(li, ul.childNodes[0]);
+        let task=document.createElement('p');
+        task.textContent=str;
+        li.append(task);
+        let done=document.createElement('button');
+        done.className="done";
+        li.append(done);
+        let tick=document.createElement('i');
+        tick.className="fa-solid fa-check fa-lg";
+        done.append(tick);
+        let remove=document.createElement('button');
+        remove.className="remove";
+        li.append(remove);
+        let cross=document.createElement('i');
+        cross.className="fa-solid fa-xmark fa-lg";
+        remove.append(cross);
+        document.getElementById('insert').value="";
+        let d= document.getElementsByClassName('done');
+        for(let i=0; i<d.length; i++){
+            d[i].addEventListener('click', function(){
+            this.parentElement.firstElementChild.setAttribute("style", "text-decoration: line-through");
+            })
+        }
+        let rem= document.getElementsByClassName('remove');
+        for(let i=0; i<rem.length; i++){
+            rem[i].addEventListener('click', function(){
+            this.parentElement.remove();
+            })
+        }
     }
-    
 }
-
-
-
